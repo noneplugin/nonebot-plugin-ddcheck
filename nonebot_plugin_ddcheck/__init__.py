704575b4557af8b16188a20a312ebe71b3f4c1f8
meetwq <meetwq@gmail.com>
2022-06-20 21:37:55 +0800

    适配插件元数据

--- nonebot_plugin_ddcheck/__init__.py
+++ nonebot_plugin_ddcheck/__init__.py
@@ -2,22 +2,25 @@ import traceback
 from loguru import logger
 from nonebot import on_command
 from nonebot.params import CommandArg
+from nonebot.plugin import PluginMetadata
 from nonebot.adapters.onebot.v11 import Message, MessageSegment
 from nonebot.log import logger
 
+from .config import Config
 from .data_source import get_reply
 
-
-__help__plugin_name__ = "ddcheck"
-__des__ = "成分姬"
-__cmd__ = """
-查成分 {用户名/UID}
-""".strip()
-__short_cmd__ = __cmd__
-__example__ = """
-查成分 小南莓Official
-""".strip()
-__usage__ = f"{__des__}\nUsage:\n{__cmd__}\nExample:\n{__example__}"
+__plugin_meta__ = PluginMetadata(
+    name="成分姬",
+    description="查询B站用户关注的VTuber成分",
+    usage="查成分 B站用户名/UID",
+    config=Config,
+    extra={
+        "unique_name": "ddcheck",
+        "example": "查成分 小南莓Official",
+        "author": "meetwq <meetwq@gmail.com>",
+        "version": "0.1.7",
+    },
+)
 
 
 ddcheck = on_command("查成分", block=True, priority=12)
